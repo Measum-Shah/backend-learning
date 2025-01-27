519cb0fea32550889111b560d59604c373f08a4e
Measum-Shah <shahmeasum008@gmail.com>
2025-01-27 23:23:32 +0500

    we made a basic server using express

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const http = require("http");
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res)=>{
-    if(req.url=="/about"){
-        res.end("About page")
-    }
-    if(req.url=="/"){
-        res.end("Home Page")
-    }
-    if(req.url=="/contact"){
-        res.end("03xxxx")}
+// consider express as a box have many tools , but it is closed . In order to use express tools we have to open box. so we call it as "express()" and store it in const app then this box get open and then we can use every tool as app.tool....
+// In express we dont have to use "createserver" the express manage it itself
+// Express also uses HTTP at backend
+
+// Keep in mind use of '=,==,===' in js in http server
+
+app.get('/',(req,res)=>{
+    res.send("Hello Learner")
 })
 
-server.listen(3000)
+app.get('/about',(req,res)=>{
+    res.send("About Page")
+})
+
+app.get('/contact',(req,res)=>{
+    res.send("contact page")
+})
+
+
+app.listen(3000);
 
 

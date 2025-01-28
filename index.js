81d22435a0e430bc4fb97bc41bf474ba01986a57
Measum-Shah <shahmeasum008@gmail.com>
2025-01-29 00:35:43 +0500

    Learned about custom middlewares

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 
-// Rendering html 
-// 1st download ejs using "npm i ejs"
-// set view engine
-// create views folder
-// make index.ejs in views folder
-// now use res.render instead of res.send
-// enter file name into "" q.m
+// Middle ware
+// if any request come to route, it should go through a function and then to route
+// you can use it as app.use((req,res,next)=>{
+    // write function here })
+
 
 app.set('view engine', 'ejs')
+
+app.use((req,res,next)=>{
+    console.log("this is middle ware")
+    // res.send("COMSIAN DEV ON TOP")
+    return next()
+})
+// output will be this is middleware whenever req pass through routes
+// you have to declare response 
+// should have return next() to convert to next function
+// only one response can be generated
 
 app.get('/',(req,res)=>{
     res.render("index")

ca65fa42633c8c69c2601a04f06b223d25f5aaf1
Measum-Shah <shahmeasum008@gmail.com>
2025-01-31 00:46:25 +0500

    learned how to render js and css of frontend to backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 // const morgan = require('morgan')
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
@@ -18,28 +19,17 @@ app.post('/get-form-data',(req,res)=>{
     res.send("Data Submitted Successfuly")
 })
 
-
-// The data comes into request.query
-// Then response will be give
-// Now paste the route in "action" of form in index.ejs
-// when we run we get output {} this because we are getting data but data don't have specific identification
-// we should name inputs in order to resolve this problem
-// After giving name output = { username: 'shazia', email: 'shazia@gmail.com', password: '12345678' }
-// The data submitted is sowing in url
-// password is senstive info it should not be shown
-// if we don't want to show our data in url we will use post route
-// post method is generally used to take data from frontend to backend
-// get method is generally used to take data from server to frontend
-// if we use post method data comes into req.body
-// to use post u must specify post method in form
-// automatically form detects get method
-// Now the output is "undefined"
-// it comes because express by default cant read req.body
-// in order to resolve this we have to use two express middlewares express.json() and express.urlencoded
-// ({extended:true})
-// Both these are BUILT IN MIDDLEWARES
-// Now The Output is  { username: 'measum', email: 'love uh', password: 'jadfa' }
-
+// Now we will attach css file with our backend
+// first make a public folder
+// as we have created index.css so we have to request one more file along with ejs
+// Static files are files which user can access witout any authentication
+// for this use app.use(express.static("folder name"))
+// Now add link tag in ejs to css file
+// run server and boom you have black background
+// similarly you can connect js file for frontend with backend 
+// mmake index.js in pblic folder add script tag in ejs
+// and boom you can use js file
+// in this way we can render static files such as css and js
 
 app.listen(3000);
 
